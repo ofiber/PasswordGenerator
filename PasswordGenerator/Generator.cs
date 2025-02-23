2499103607be9e6d96fd93976344d185ec002804
ofiber <61956298+ofiber@users.noreply.github.com>
2025-02-22 20:47:50 -0500

    Add project files.

---
+++ PasswordGenerator/Generator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class Generate
+    {
+        //public static string GetOptions()
+        //{
+        //    Console.WriteLine("How long would you like your password to be? (8-128 characters)");
+        //    int length = int.Parse(Console.ReadLine());
+
+        //    Console.WriteLine("Would you like to use symbols? (y/n)");
+        //    bool useSymbols = Console.ReadLine().ToLower() == "y" ? true : false;
+
+        //    Console.WriteLine("Would you like to use uppercase letters? (y/n)");
+        //    bool useUpper = Console.ReadLine().ToLower() == "y" ? true : false;
+
+        //    Console.WriteLine("Would you like to use lowercase letters? (y/n)");
+        //    bool useLower = Console.ReadLine().ToLower() == "y" ? true : false;
+
+        //    Console.WriteLine("Would you like to use numbers? (y/n)");
+        //    bool useNumbers = Console.ReadLine().ToLower() == "y" ? true : false;
+
+        //    return GeneratePassword(length, useSymbols, useUpper, useLower, useNumbers);
+        //}
+
+        public static string GetPassword(int length, bool useSymbols, bool useUpper, bool useLower, bool useNumbers)
+        {
+            return GeneratePassword(length, useSymbols, useUpper, useLower, useNumbers);
+        }
+
+        private static string GeneratePassword(int length, bool useSymbols, bool useUpper, bool useLower, bool useNumbers)
+        {
+            string password = "";
+            string symbols = "!@#$%^&*()_+{}|:<>?~";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "0123456789";
+
+            Random random = SeedRandom();
+
+            for (int i = 0; i < (length / 4); i++)
+            {
+                if (useSymbols)
+                {
+                    password += symbols[random.Next(0, symbols.Length)];
+                }
+                if (useUpper)
+                {
+                    password += upper[random.Next(0, upper.Length)];
+                }
+                if (useLower)
+                {
+                    password += lower[random.Next(0, lower.Length)];
+                }
+                if (useNumbers)
+                {
+                    password += numbers[random.Next(0, numbers.Length)];
+                }
+            }
+
+            password = Shuffle(password, random);
+
+            return password;
+        }
+
+        private static string Shuffle(string password, Random r)
+        {
+            char[] array = password.ToCharArray();
+            int n = array.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = r.Next(n + 1);
+                char value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+
+            return new string(array);
+        }
+
+        private static Random SeedRandom()
+        {
+            TimeOnly time = TimeOnly.FromDateTime(DateTime.Now);
+            DateOnly date = DateOnly.FromDateTime(DateTime.Now);
+
+            int hash1 = time.GetHashCode();
+            int hash2 = date.GetHashCode();
+
+            int seed = HashCode.Combine(hash1, hash2);
+
+            Random random = new Random(seed);
+
+            return random;
+        }
+
+
+    }
+}
---
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PasswordGenerator;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            bool useSymbols = (bool)chkSpecialCharacters.IsChecked ? true : false;
+            bool useUpper = (bool)chkUpperCase.IsChecked ? true : false;
+            bool useLower = (bool)chkLowerCase.IsChecked ? true : false;
+            bool useNumbers = (bool)chkNumbers.IsChecked ? true : false;
+
+            int length = int.Parse(txtPasswordLength.Text);
+
+            HideOptions();
+
+            string password = Generate.GetPassword(length, useSymbols, useUpper, useLower, useNumbers);
+
+            txtPassword.Text = password;
+        }
+
+        private void HideOptions()
+        {
+            lblPassword.Visibility = Visibility.Visible;
+            txtPassword.Visibility = Visibility.Visible;
+            btnGenerate.Visibility = Visibility.Hidden;
+            lblPasswordLength.Visibility = Visibility.Hidden;
+            txtPasswordLength.Visibility = Visibility.Hidden;
+            chkSpecialCharacters.Visibility = Visibility.Hidden;
+            chkUpperCase.Visibility = Visibility.Hidden;
+            chkLowerCase.Visibility = Visibility.Hidden;
+            chkNumbers.Visibility = Visibility.Hidden;
+        }
+    }
+}
